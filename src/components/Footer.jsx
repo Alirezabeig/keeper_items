6392b6996aa5d6db80f848c4204efc5c2b54a052
Alireza B <Alireza@simor.org>
2020-07-13 17:40:27 -0600

    added map function to go through notes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,11 @@ function Footer(){
 
   const currentYear = new Date().getFullYear();
   return (
+    <footer>
     <p>
     copyright C {currentYear}
-    </p>  
+    </p>
+    </footer>
   );
 }
 export default Footer;
---
+++ src/notes.js
@@ -0,0 +1,25 @@
+const notes = [{
+    key : 1,
+    title : "my first to do",
+    content : " Content ##"
+
+  },
+
+  {
+    key : 2,
+    title : "my first to do",
+    content : " Content ##"
+
+  },
+
+  {
+
+    key : 3,
+      title : "my first to do",
+      content : " Content ##"
+
+
+  },
+
+];
+export default notes;
