e05bbfb7de88e4d7043cc9128b2129c9936ebe24
Alireza B <Alireza@simor.org>
2020-07-15 14:48:46 -0600

    add post completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
-import notes from '../notes'
-
+import CreateArea from './CreateArea';
+import notes from '../notes';
 
 
 function App(){
+  const [notes, setNote] = useState([]);
+
+  function addNote(newNote){
+    setNote(prevNote => {
+      return [...prevNote,newNote];
+    })
+  }
+
 return (
   <div>
    <Header />
-   {notes.map(noteItem =>
-     <Note
-        key={noteItem.key}
-        title={noteItem.title}
-        content={noteItem.content}
-        />
-   )}
+    <CreateArea onAdd={addNote} />
+
+      {notes.map((noteItem) => {
+        return <Note title={noteItem.title} content={noteItem.content}/>
+      })}
+
    <Footer />
   </div>
   );
